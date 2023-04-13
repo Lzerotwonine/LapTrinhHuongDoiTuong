ca06564d7bd5386053653b3ef99892afea31afd8
Lzerotwonine <125208234+Lzerotwonine@users.noreply.github.com>
2023-04-13 22:55:39 +0700

    Update SanPham.java

--- LAB8/src/lab4c1/SanPham.java
+++ LAB8/src/lab4c1/SanPham.java
@@ -7,13 +7,14 @@ public class SanPham {
     public double donGia;
     public double giamGia;
     
-    public void nhap() {
+    public void nhap() 
+        Scanner in = new Scanner(System.in);
         System.out.println("\nTen san pham: ");
-        tenSp = new Scanner(System.in).nextLine();
+        tenSp = in.nextLine();
         System.out.println("Don gia: ");
-        donGia = new Scanner(System.in).nextDouble();
+        donGia = in.nextDouble();
         System.out.println("Giam gia: ");
-        giamGia = new Scanner(System.in).nextDouble();
+        giamGia = in.nextDouble();
     }
     
     public void xuat() {
