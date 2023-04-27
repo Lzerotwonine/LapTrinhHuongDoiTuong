3ca6d496da64ac4535dd324636c21ac1a4a2a562
Lzerotwonine <125208234+Lzerotwonine@users.noreply.github.com>
2023-04-27 21:55:34 +0700

    Update Main.java

--- LAB1TH/LAB2/Main.java
+++ LAB1TH/LAB2/Main.java
@@ -2,6 +2,6 @@ package lab2;
 
 public class Main {
     public static void main(String[] args) {
-        LopHocPhan lhp = new LopHocPhan("123456", "LT Huong doi tuong", "Nguyen Van A", "Thu 7, tiet 4-6, phong A1.1", );
+        LopHocPhan lhp = new LopHocPhan();
     }
 }
